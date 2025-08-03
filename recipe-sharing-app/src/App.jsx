07a9d37454e515249e29a053a72b7036b03a1d7d
Dorwu Gabriel <mathesond.gabriel90@gmail.com>
2025-08-03 19:37:18 +0100

    Updates
    Updated App.jsx and main.jsx to include the Router function in the App.jsx file.

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,26 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipe from './components/addRecipe';
 import RecipeDetails from './components/RecipeDetails';
 
 const App = () => {
   return (
-    <div style={{ maxWidth: '600px', margin: '2rem auto' }}>
-      <h1>Recipe Sharing App</h1>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipe />
-              <hr />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div style={{ maxWidth: '600px', margin: '2rem auto' }}>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipe />
+                <hr />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
