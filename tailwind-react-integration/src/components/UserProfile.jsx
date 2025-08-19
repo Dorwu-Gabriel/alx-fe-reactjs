39ed222564bcadcfd5235375707b6e05c1bf7af5
Dorwu Gabriel <mathesond.gabriel90@gmail.com>
2025-08-19 17:10:55 +0000

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,12 +2,12 @@ function UserProfile() {
     return (
       <div className="bg-gray-100 md:p-8 sm:p-4 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg shadow-blue-500 hover:shadow-2xl hover:shadow-blue-600 transition-all duration-300 ease-in-out transform hover:-translate-y-1">
         <img 
-          className="rounded-full w-36 h-36 mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110 hover:shadow-lg"
+          className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110 hover:shadow-lg"
           src="https://img.icons8.com/?size=100&id=11795&format=png&color=000000" 
           alt="User" 
         />
-        <h1 className="text-xl font-bold text-center text-blue-800 my-4 transition-colors duration-300 hover:text-blue-600">John Doe</h1>
-        <p className="text-gray-600 text-base text-center transition-colors duration-300 hover:text-gray-800">
+        <h1 className="sm:text-lg md:text-xl font-bold text-center text-blue-800 my-4 transition-colors duration-300 hover:text-blue-600">John Doe</h1>
+        <p className="text-gray-600 sm:text-sm md:text-base text-center transition-colors duration-300 hover:text-gray-800">
           Developer at Example Co. <br />Loves to write code and explore new technologies.
         </p>
       </div>
