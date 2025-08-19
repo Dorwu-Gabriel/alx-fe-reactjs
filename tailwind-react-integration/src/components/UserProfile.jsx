02189da6d6f72e624c244d93fa6edc94f8d43686
Dorwu Gabriel <mathesond.gabriel90@gmail.com>
2025-08-19 17:01:06 +0000

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg shadow-blue-500 hover:shadow-2xl hover:shadow-blue-600 transition-all duration-300 ease-in-out transform hover:-translate-y-1">
+      <div className="bg-gray-100 md:p-8 sm:p-4 max-w-sm mx-auto my-20 rounded-lg shadow-lg shadow-blue-500 hover:shadow-2xl hover:shadow-blue-600 transition-all duration-300 ease-in-out transform hover:-translate-y-1">
         <img 
           className="rounded-full w-36 h-36 mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110 hover:shadow-lg"
           src="https://img.icons8.com/?size=100&id=11795&format=png&color=000000" 
